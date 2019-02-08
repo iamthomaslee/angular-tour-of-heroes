3e1ea6b2a5a6b52a1d59bcc1ef73991eda1429fa
Thomas Lee <leez79@gmail.com>
2019-02-08 15:23:13 -0800

    Added hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,15 +10,14 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  
   constructor(private heroService: HeroService) {}
+  
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
+  
   ngOnInit() {
     this.getHeroes();
   }
