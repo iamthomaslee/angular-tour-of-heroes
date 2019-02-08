9850721f1a8b131a618ff212c2c202416aa7f6df
Thomas Lee <thomas.lee@quadrantsoftware.com>
2019-02-08 09:58:38 -0800

    Splitted compoment into master and detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,16 +7,13 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
   constructor() { }
 
   ngOnInit() {
